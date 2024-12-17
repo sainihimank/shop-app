310d9163fba18a644d6507ae526fd65baf4f59d7
sainihimank <114724317+sainihimank@users.noreply.github.com>
2024-12-17 14:16:12 +0530

    Add files via upload

---
+++ src/app/Coponents/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import RightCartIcon from "../Cart/RightCartIcon";
+
+function Footer() {
+  return (
+    <Fragment>
+      <RightCartIcon />
+      <div className="text-center bg-dark text-white py-2 fixed-bottom">
+        <span>
+          Find all the your needs here
+        </span>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Footer;
